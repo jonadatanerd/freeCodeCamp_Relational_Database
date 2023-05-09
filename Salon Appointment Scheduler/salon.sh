3a4a9f7245fea68a22608936654a14ed187bdbbc
Jonathan Vásquez <131181758+jonadatanerd@users.noreply.github.com>
2023-05-09 14:19:21 -0400

    Create salon.sh

---
+++ Salon Appointment Scheduler/salon.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+PSQL="psql -X --username=freecodecamp --dbname=salon --no-align --tuples-only -c"
+
+echo -e "\n~~~~~ NKP1111 SALON ~~~~~\n"
+
+MAIN_MENU(){
+
+  if [[ $1 ]]
+  then 
+    echo -e "\n$1"
+  else
+    echo -e "\nWelcome to nkp1111 salon, How can i help you"
+  fi
+
+    
+  SERVICES 
+}
+
+SERVICES(){
+  # get services
+  SERVICES=$($PSQL "SELECT service_id, name FROM services")
+  echo "$SERVICES" | while IFS="|" read SERVICE_ID SERVICE_NAME
+  do 
+    echo "$SERVICE_ID) $SERVICE_NAME"
+  done
+  read SERVICE_ID_SELECTED
+
+  # if not service id
+  if [[ ! $SERVICE_ID_SELECTED =~ ^[0-9]+$ ]]
+  then
+    # send to main menu
+    MAIN_MENU "I could not find that service. What would you like today?"
+
+  else
+    # get customer info
+    echo -e "\nWhat's your phone number?"
+    read CUSTOMER_PHONE
+
+    # check if customer exist
+    CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone = '$CUSTOMER_PHONE'")
+    
+    # if not exist 
+    if [[ -z $CUSTOMER_NAME ]]
+    then
+      # create new customer
+      echo "I don't have a record for that phone number, what's your name?"
+      read CUSTOMER_NAME
+      # insert new customer
+      INSERT_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers (name, phone) VALUES ('$CUSTOMER_NAME', '$CUSTOMER_PHONE')")
+    fi
+
+    # get service name
+    SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id = $SERVICE_ID_SELECTED")
+    # get customer_id
+    CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE name = '$CUSTOMER_NAME'")
+    
+    # get service time
+    echo "What time would you like your $SERVICE_NAME, $CUSTOMER_NAME?"
+    read SERVICE_TIME   
+
+    # insert appointment
+    INSERT_INTO_APPOINTMENT=$($PSQL "INSERT INTO appointments (customer_id, service_id, time) VALUES ('$CUSTOMER_ID', $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
+    if [[ $INSERT_INTO_APPOINTMENT == 'INSERT 0 1' ]]
+    then
+      # send to main menu
+      echo -e "\nI have put you down for a $SERVICE_NAME at $SERVICE_TIME, $CUSTOMER_NAME."
+      EXIT
+    fi
+  fi
+}
+
+EXIT(){
+  echo -e "\nThank you for visiting. Please come again.\n"
+}
+
+MAIN_MENU
